efd42a74f6d4b8a8ad5dfdb5c6d6534ee286b549
Blepp <dixon.verrix13@gmail.com>
2016-08-20 14:30:30 +0700

    Add feature terbilang

--- Documents/NetBeansProjects/marathongit2/src/Marathongit2/Terbilang.java
+++ Documents/NetBeansProjects/marathongit2/src/Marathongit2/Terbilang.java
@@ -1,13 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package Marathongit2;
 
-/**
- *
- * @author UPHM
- */
 public class Terbilang {
       public static String proses(int value) {
         String[] bilangan = {"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas"};
---
+++ Documents/NetBeansProjects/marathongit2/src/Marathongit2/Woo.java
@@ -0,0 +1,16 @@
+package Marathongit2;
+
+import static Marathongit2.Terbilang.proses;
+
+
+
+
+
+public class Woo {
+
+
+    public static void main(String[] args) {    
+        System.out.println(proses(27400));
+    }
+    
+}
